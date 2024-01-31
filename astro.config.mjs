40bc6b0b840f15bf6bbf6d1628ab4ddc7f00c26e
ingrid1802 <ingridaybar841@gmail.com>
2024-01-31 00:00:09 -0500

    prueba20

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: './',
+	base: '/starling/docs/',
 	outDir: './docs',
 	integrations: [
 		starlight({
